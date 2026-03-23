bd44d695e605ca6f024a6c33ae58bd3364ff0300
Govind <govindnacharya@gmail.com>
2026-03-23 11:40:45 +0530

    first commit

---
+++ src/main/java/com/example/demo/UberBackendAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBackendAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberBackendAuthApplication.class, args);
+	}
+
+}
